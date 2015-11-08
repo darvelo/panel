1518b1dbd9149a2dc8092d8d7e7c47bae818da98
David Arvelo <david@davidarvelo.com>
2015-11-07 22:11:44 -0500

    Modify installation for Debian

--- __main__.py
+++ __main__.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
+
 import os
+import sys
 from libinstall import FileInstaller, PackageInstaller
+
+
 dir = os.path.dirname(__file__)
 
-PackageInstaller.try_install('python-pyqt5')     # for panel
-PackageInstaller.try_install('dmenu')            # program executor
-PackageInstaller.try_install('bspwm-git')        # window manager
-PackageInstaller.try_install('sxhkd-git')        # hotkey manager
-PackageInstaller.try_install('xdo')              # like xdotool, but different
+if not FileInstaller.has_executable('bspwm'):
+    print('Missing bspwm! Exiting...')
+    sys.exit(1)
+if not FileInstaller.has_executable('sxhkd'):
+    print('Missing sxhkd! Exiting...')
+    sys.exit(1)
+
+# Debian packages
+PackageInstaller.try_install('python3-pyqt5')     # for panel
+PackageInstaller.try_install('python3-pyqt5.qtwebkit')     # for panel
+PackageInstaller.try_install('python3-psutil')       # CPU usage
+PackageInstaller.try_install('python3-xlib') # window titles
+PackageInstaller.try_install('suckless-tools')            # program executor
 PackageInstaller.try_install('feh')              # wallpaper renderer
 
-PackageInstaller.try_install('psutil', method='pip')       # CPU usage
+# Pip packages
 PackageInstaller.try_install('pyalsaaudio', method='pip')  # system volume
 PackageInstaller.try_install('python-mpd2', method='pip')  # mpd interaction
-PackageInstaller.try_install('python3-xlib', method='pip') # window titles
+# PackageInstaller.try_install('psutil', method='pip')       # CPU usage
+# PackageInstaller.try_install('python3-xlib', method='pip') # window titles
 
-FileInstaller.create_symlink(os.path.join(dir, 'sxhkdrc'), '~/.config/sxhkd/sxhkdrc')
-FileInstaller.create_symlink(os.path.join(dir, 'bspwmrc'), '~/.config/bspwm/bspwmrc')
-FileInstaller.create_symlink(os.path.join(dir, 'toggle-fullscreen'), '~/.config/bspwm/toggle-fullscreen')
-FileInstaller.create_symlink(os.path.join(dir, 'rules'), '~/.config/bspwm/rules')
-FileInstaller.create_symlink(os.path.join(dir, 'panel'), '~/.config/bspwm/panel')
+# Files to link
+# FileInstaller.create_symlink(os.path.join(dir, 'sxhkdrc'), '~/.config/sxhkd/sxhkdrc')
+# FileInstaller.create_symlink(os.path.join(dir, 'bspwmrc'), '~/.config/bspwm/bspwmrc')
+# FileInstaller.create_symlink(os.path.join(dir, 'toggle-fullscreen'), '~/.config/bspwm/toggle-fullscreen')
+# FileInstaller.create_symlink(os.path.join(dir, 'rules'), '~/.config/bspwm/rules')
+# FileInstaller.create_symlink(os.path.join(dir, 'panel'), '~/.config/bspwm/panel')
